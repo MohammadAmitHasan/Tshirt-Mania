b3dfaf41b92a594827b9aba1fedc16bace6a1893
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-01 22:01:43 +0600

    Some active link styles added

--- src/Components/CustomLink/CustomLink.js
+++ src/Components/CustomLink/CustomLink.js
@@ -6,7 +6,7 @@ function CustomLink({ children, to, ...props }) {
     return (
         <div className='inline'>
             <Link className='px-2 py-1 hover:bg-red-400 mx-2 rounded'
-                style={{ backgroundColor: match ? 'blue' : '', }}
+                style={{ color: match ? 'lime' : '', border: match ? '2px solid white' : '' }}
                 to={to}
                 {...props}
             >
